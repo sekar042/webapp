1a8843d9dba3c483edb2bd85a5d6c6dd08e39ba2
sekar042 <67045400+sekar042@users.noreply.github.com>
2021-04-27 19:46:19 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -4,14 +4,7 @@ OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
 
-if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
-    sudo mkdir -p "$DATA_DIRECTORY"
-    sudo chmod -R 777 "$DATA_DIRECTORY"
 
-    sudo mkdir -p "$REPORT_DIRECTORY"
-    sudo chmod -R 777 "$REPORT_DIRECTORY"
-fi
 
 # Make sure we are using the latest version
 docker pull owasp/dependency-check
