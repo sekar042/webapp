8baeb0d36f22024ece9f72ea818b6264b4c2ab93
sekar042 <67045400+sekar042@users.noreply.github.com>
2021-04-27 20:03:59 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -3,9 +3,6 @@
 OWASPDC_DIRECTORY="$HOME/OWASP-Dependency-Check"
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
-
-docker pull owasp/dependency-check
-
 docker run --rm \
     --volume $(pwd):/src \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
