936b18552f935c30331062e34aaede0dde8626b8
sekar042 <67045400+sekar042@users.noreply.github.com>
2021-04-27 19:44:18 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -6,11 +6,11 @@ REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directories"
-    mkdir -p "$DATA_DIRECTORY"
-    chmod -R 777 "$DATA_DIRECTORY"
+    sudo mkdir -p "$DATA_DIRECTORY"
+    sudo chmod -R 777 "$DATA_DIRECTORY"
 
-    mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
+    sudo mkdir -p "$REPORT_DIRECTORY"
+    sudo chmod -R 777 "$REPORT_DIRECTORY"
 fi
 
 # Make sure we are using the latest version
